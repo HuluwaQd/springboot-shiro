6f353669aca5a848739e98e3575f7118dac7e1cb
liuyouwen <641359145@qq.com>
2021-04-21 18:49:59 +0800

    1

--- src/main/java/com/example/springbootshiro/config/ShiroConfig.java
+++ src/main/java/com/example/springbootshiro/config/ShiroConfig.java
@@ -1,14 +1,13 @@
 package com.example.springbootshiro.config;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authz.Authorizer;
-import org.apache.shiro.authz.ModularRealmAuthorizer;
-import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author lyw
@@ -17,23 +16,43 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ShiroConfig {
     @Bean
-    public SecurityManager initSecurityManager(UserRealm userRealm){
+    public DefaultWebSecurityManager initSecurityManager(Realm realm){
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(userRealm);
-        securityManager.setRememberMeManager(null);
-        SecurityUtils.setSecurityManager(securityManager);
+        securityManager.setRealm(realm);
         return securityManager;
     }
 
     @Bean
-    public UserRealm initUserRealm(){
+    public Realm initUserRealm(){
         UserRealm userRealm = new UserRealm();
         return userRealm;
     }
 
+    @Bean("shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager defaultWebSecurityManager) {
+        ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+        shiroFilter.setSecurityManager(defaultWebSecurityManager);
 
-    @Bean
-    public Authorizer authorizer(){
-        return new ModularRealmAuthorizer();
+
+        shiroFilter.setLoginUrl("/login.html");
+        shiroFilter.setUnauthorizedUrl("/");
+
+        Map<String, String> filterMap = new LinkedHashMap<>();
+        filterMap.put("/swagger/**", "anon");
+        filterMap.put("/v2/api-docs", "anon");
+        filterMap.put("/swagger-ui.html", "anon");
+        filterMap.put("/webjars/**", "anon");
+        filterMap.put("/swagger-resources/**", "anon");
+
+        filterMap.put("/login.html", "anon");
+        filterMap.put("/sys/login", "anon");
+        filterMap.put("/favicon.ico", "anon");
+        filterMap.put("/captcha.jpg", "anon");
+
+        filterMap.put("/**", "authc");
+        shiroFilter.setFilterChainDefinitionMap(filterMap);
+
+        return shiroFilter;
     }
+
 }
