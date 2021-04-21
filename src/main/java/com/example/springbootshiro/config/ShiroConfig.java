4220cdffdc64d2b15814a7379b7b9c62018396b6
liuyouwen <641359145@qq.com>
2021-04-21 23:50:42 +0800

    shiro用户认证完成，token待测

--- src/main/java/com/example/springbootshiro/config/ShiroConfig.java
+++ src/main/java/com/example/springbootshiro/config/ShiroConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
